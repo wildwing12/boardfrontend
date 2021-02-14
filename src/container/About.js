26c6dc86369042c2039e5a59c3dda5c303f6aa32
wildwing <ggipk1010@gmail.com>
2021-02-14 15:38:39 +0900

    create-front_end

---
+++ src/container/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            여기는 about입니다.
+        </div>
+    )
+}
---
+++ src/container/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <div>
+            홈페이지 입니다.
+        </div>
+    )
+}
